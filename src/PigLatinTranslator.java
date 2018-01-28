2336c1f40fe797ab0fc67b4c8443fbdfa2ccb9f5
davmusko <davmusko@gmail.com>
2018-01-28 02:59:11 -0500

    rerun feature

--- src/PigLatinTranslator.java
+++ src/PigLatinTranslator.java
@@ -1,20 +1,31 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class PigLatinTranslator {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		String reRun;
 		String userPhrase;
 		String[] userPhraseSplit;
 		String[] userPhraseInPig;
 		
 		System.out.println("Welcome to the Pig Latin Translator!");
 		
-		Scanner scan = new Scanner(System.in);
-		userPhrase = TranslatorApp.lowerCaser(scan);
-		userPhraseSplit = TranslatorApp.phraseSplitter(userPhrase);
-		userPhraseInPig = TranslatorApp.toPigLatiner(userPhraseSplit);
-		System.out.print(Arrays.toString(userPhraseInPig));
-		scan.close();
+		do {
+			Scanner scan = new Scanner(System.in);
+			userPhrase = TranslatorApp.lowerCaser(scan);
+			userPhraseSplit = TranslatorApp.phraseSplitter(userPhrase);
+			userPhraseInPig = TranslatorApp.toPigLatiner(userPhraseSplit);
+			
+			
+			for (int i = 0; i < userPhraseInPig.length; i++) {
+				System.out.print(userPhraseInPig[i] + " ");
+			}
+			System.out.println("\nWould you like to translate another word/phrase? (esyay/onay): ");
+			reRun = scan.nextLine();
+			
+		} while(reRun.equalsIgnoreCase("esyay")|| reRun.equalsIgnoreCase("yes")|| reRun.equalsIgnoreCase("y"));
+		
+		
+		
 	}
 
 
